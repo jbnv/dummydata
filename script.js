992541c414fabd80a44d9b20209032d324bbede2
Jay Bienvenu <bienvenunet@yahoo.com>
2015-12-03 21:11:03 -0600

    Refactor code for commonality.

--- script.js
+++ script.js
@@ -36,23 +36,24 @@ function toTitleCase(str)
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
-function setActiveElementValue(value) {
-  var script = 'var form = document.activeElement;form.value = (form.value + "' + value + '");';
-    chrome.tabs.executeScript({code : script});
+function number(){
+  return Math.floor(Math.pow(10,Math.random()*10));
 }
 
-function insertNumber(info, tab){
-  var number = Math.floor(Math.pow(10,Math.random()*10));
-  setActiveElementValue(number);
-}
-
-function insertAddress(info, tab){
+function streetAddress(){
   var number = Math.floor(Math.pow(Math.random()*4,10));
   var types = ["Street","Road","Drive","Lane"];
   var address = "" + number + " " + nextDatum('EnglishOrdinalNumbers') + " " + types[Math.floor(types.length*Math.random())]
-  address = toTitleCase(address);
-  setActiveElementValue(address);
+  return toTitleCase(address);
 }
 
-var a = chrome.contextMenus.create({"title": "Number", "contexts":["editable"], "onclick": insertNumber});
-var b = chrome.contextMenus.create({"title": "Street Address", "contexts":["editable"], "onclick": insertAddress});
+function insert(generatorFn,options) {
+  return function(info, tab) {
+    var value = generatorFn(options);
+    var script = 'var form = document.activeElement;form.value = (form.value + "' + value + '");';
+    chrome.tabs.executeScript({code : script});
+  }
+}
+
+var a = chrome.contextMenus.create({"title": "Number", "contexts":["editable"], "onclick": insert(number)});
+var b = chrome.contextMenus.create({"title": "Street Address", "contexts":["editable"], "onclick": insert(streetAddress)});
