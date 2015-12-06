320f330d3aecf27cf00635581b1fa0235bb603ff
Jay Bienvenu <bienvenunet@yahoo.com>
2015-12-05 22:50:53 -0600

    Spanish: Add name stems.

--- spanish.js
+++ spanish.js
@@ -3,20 +3,43 @@ var Spanish = function() {
   downloadTextFile('SpanishMaleNames');
   downloadTextFile('SpanishFemaleNames');
   downloadTextFile('SpanishSurnames');
+  downloadTextFile('SpanishNameStems');
 
   this.maleName = function() {
-    return nextDatum('SpanishMaleNames');
+    if (Math.random() < 0.5) {
+      return nextDatum('SpanishMaleNames');
+    } else {
+      return nextDatum('SpanishNameStems')+"o";
+    }
   };
 
   this.femaleName = function() {
-    return nextDatum('SpanishFemaleNames');
+    if (Math.random() < 0.5) {
+      return nextDatum('SpanishFemaleNames');
+    } else {
+      return nextDatum('SpanishNameStems')+"a";
+    }
+  };
+
+  var oneSurname = function() {
+    if (Math.random() < 0.5) {
+      return nextDatum('SpanishSurnames');
+    } else {
+      var stem = nextDatum('SpanishNameStems');
+      console.log(stem,stem.substr(-1))
+      switch (stem.substr(-1)) {
+        case "c": return stem.substr(0,stem.length-1)+"quez";
+        case "g": return stem.substr(0,stem.length-1)+"guez";
+      }
+      return stem+"ez";
+    }
   };
 
   this.surname = function() {
     if (Math.random() < 0.9) {
-      return nextDatum('SpanishSurnames');
+      return oneSurname();
     } else {
-      return nextDatum('SpanishSurnames')+" y "+nextDatum('SpanishSurnames');
+      return oneSurname()+" y "+oneSurname();
     };
   };
 
