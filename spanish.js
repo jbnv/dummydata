c18fefbd4e53bae9f4664fc156ba50b6beb11cda
Jay Bienvenu <bienvenunet@yahoo.com>
2015-12-08 21:33:52 -0600

    Further name refinements.

--- spanish.js
+++ spanish.js
@@ -10,58 +10,63 @@ var Spanish = function() {
 
   var _this = this;
 
-  function adjustNameStem(stem) {
-    switch (stem.substr(-1)) {
-      case "c": return stem.substr(0,stem.length-1)+"qu";
-      case "g": return stem.substr(0,stem.length-1)+"gu";
+  function randomNameSuffix() { return [[3,""],[1,"it"],[1,"in"],[1,"ill"]].randomWeightedElement(); }
+
+  function appendSuffix(stem,suffix) {
+    if (suffix == null || suffix == "") return stem;
+    if (suffix.substr(0,1) == "i" || suffix.substr(0,1) == "e") {
+      switch (stem.substr(-1)) {
+        case "c": stem = stem.substr(0,stem.length-1)+"qu"; break;
+        case "g": stem = stem+"u"; break;
+      }
     }
-    return stem;
+    return stem+suffix;
+  }
+
+  function properNameStem() {
+    var stem = nextDatum('SpanishNameStems',{transform:toInitialCase});
+    var suffix = randomNameSuffix();
+    return appendSuffix(stem,suffix);
   }
 
   function ordinalNumberAsName() {
-    return nextDatum('SpanishOrdinalNumbers',{transform:toInitialCase});
+    var stem = nextDatum('SpanishOrdinalNumbers',{transform:toInitialCase});
+    var suffix = randomNameSuffix();
+    return appendSuffix(stem,suffix);
   }
 
-  this.maleName = function() {
-    return [
-      [3, function() { return nextDatum('SpanishMaleNames'); }],
-      [3, function() { return nextDatum('SpanishNameStems')+"o"; }],
-      [1, function() { return adjustNameStem(nextDatum('SpanishNameStems'))+"ito"; }],
-      [1, function() { return adjustNameStem(nextDatum('SpanishNameStems'))+"ino"; }],
-      [1, function() { return ordinalNumberAsName()+"o"; }],
-      [1, function() { return ordinalNumberAsName()+"ito"; }],
-      [1, function() { return ordinalNumberAsName()+"ino"; }]
-    ].randomWeightedElement()();
-  };
+  var maleNameSelector = [
+    [3, function() { return nextDatum('SpanishMaleNames'); }],
+    [3, function() { return properNameStem()+"o"; }],
+    [1, function() { return ordinalNumberAsName()+"o"; }]
+  ];
 
-  this.femaleName = function() {
-    return [
-      [3, function() { return nextDatum('SpanishFemaleNames'); }],
-      [3, function() { return nextDatum('SpanishNameStems')+"a"; }],
-      [1, function() { return adjustNameStem(nextDatum('SpanishNameStems'))+"ita"; }],
-      [1, function() { return adjustNameStem(nextDatum('SpanishNameStems'))+"ina"; }],
-      [1, function() { return ordinalNumberAsName()+"a"; }],
-      [1, function() { return ordinalNumberAsName()+"ita"; }],
-      [1, function() { return ordinalNumberAsName()+"ina"; }]
-    ].randomWeightedElement()();
-  };
+  this.maleName = function() { return maleNameSelector.randomWeightedElement()(); }
 
-  var oneSurname = function() {
-    return [
-      [3, function() { return nextDatum('SpanishSurnames'); }],
-      [3, function() { return adjustNameStem(nextDatum('SpanishNameStems'))+"ez"; }],
-      [1, function() { return adjustNameStem(nextDatum('SpanishNameStems'))+"inez"; }],
-      [1, function() { return adjustNameStem(nextDatum('SpanishNameStems'))+"itez"; }],
-      [1, function() { return ordinalNumberAsName()+"ez"; }],
-      [1, function() { return ordinalNumberAsName()+"itez"; }],
-      [1, function() { return ordinalNumberAsName()+"inez"; }],
-      [1, function() { return "San "+_this.maleName(); }],
-      [1, function() { return "Santa "+_this.femaleName(); }],
-      [1, function() { return "de "+nextDatum('SpanishPlaces'); }],
-      [1, function() { return "de San "+_this.maleName(); }],
-      [1, function() { return "de Santa "+_this.femaleName(); }]
-    ].randomWeightedElement()();
-  };
+  var femaleNameSelector = [
+    [3, function() { return nextDatum('SpanishFemaleNames'); }],
+    [3, function() { return properNameStem()+"a"; }],
+    [1, function() { return ordinalNumberAsName()+"a"; }]
+  ];
+
+  this.femaleName = function() { return femaleNameSelector.randomWeightedElement()(); }
+
+  var surnameSuffixes = ["o","a","os","as","es","ez"];
+
+  function surnameSuffix() { return surnameSuffixes.randomElement(); }
+
+  var surnameSelector = [
+    [3, function() { return nextDatum('SpanishSurnames'); }],
+    [3, function() { return appendSuffix(properNameStem(),surnameSuffix()); }],
+    [1, function() { return appendSuffix(ordinalNumberAsName(),surnameSuffix()); }],
+    [1, function() { return "San "+_this.maleName(); }],
+    [1, function() { return "Santa "+_this.femaleName(); }],
+    [1, function() { return "de "+nextDatum('SpanishPlaces'); }],
+    [1, function() { return "de San "+_this.maleName(); }],
+    [1, function() { return "de Santa "+_this.femaleName(); }]
+  ];
+
+  var oneSurname = function() { return surnameSelector.randomWeightedElement()(); }
 
   this.surname = function() {
     if (Math.random() < 0.9) {
