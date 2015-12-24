3524f2ea009a1c2a3ada08c010e567ae9275fea1
Jay Bienvenu <github@bienv.com>
2015-12-24 17:04:50 -0600

    Rewrite options in Knockout. Prepare for dynamic demo. (Incomplete.) #10 #12 #13 #21

--- options.js
+++ options.js
@@ -1,24 +1,57 @@
-console.log("BEGIN options setup");
+function viewModel() {
 
-$languages = $("#languages");
-$countries = $("#countries");
-$ordinal = $("#ordinal");
+  var _this = this;
 
-$languages.val(localStorage["language"] || "English");
-$countries.val(localStorage["country"] || "UnitedStates");
-$ordinal.val(localStorage["ordinal"] || 0);
+  this.language = ko.pureComputed({
+    read: function() {
+      return localStorage["language"] || "English";
+    },
+    write: function(value) {
+      localStorage["language"] = value;
+    }
+  });
 
-$("#options-form").submit(function() {
+  this.country = ko.pureComputed({
+    read: function() {
+      return localStorage["country"] || "UnitedStates";
+    },
+    write: function(value) {
+      localStorage["country"] = value;
+    },
+  });
 
-  localStorage["language"] = $languages.val();
-  localStorage["country"] = $countries.val();
+  this.ordinal = ko.pureComputed({
+    read: function() {
+      return localStorage["ordinal"] || 0;
+    },
+    write: function(seed) {
+      var value = parseFloat(localStorage["ordinal"]);
+      if (seed) value = parseFloat(seed);
+      if (Number.isNaN(value)) value = seed || 0;
+      localStorage["ordinal"] = value;
+    },
+  });
 
-  var seed = $ordinal.val();
-  var value = parseFloat(localStorage["ordinal"]);
-  if (seed) value = parseFloat(seed);
-  if (Number.isNaN(value)) value = seed || 0;
-  localStorage["ordinal"] = value;
+  this.languageOptions = ko.observableArray(["English","German","Greek","Japanese","Spanish"]);
 
+  this.countryOptions = ko.observableArray();
+
+  // Populate the countryOptions observable.
+  [
+    ["UnitedStates","United States (USA) / Canada"],
+    "Australia",
+    "Mexico",
+    ["UnitedKingdom","United Kingdom (UK)"]
+  ].forEach(function (item) {
+    if (item.constructor === Array) {
+      outbound = {value: item[0], name: item[1]};
+    } else {
+      outbound = {value: item, name: item};
+    }
+    _this.countryOptions.push(outbound);
+  });
+}
+
+$(function() {
+  ko.applyBindings(new viewModel());
 });
-
-console.log("END options setup");
