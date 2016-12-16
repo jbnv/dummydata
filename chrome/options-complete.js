b7c832018fab59b950dc43af92c5800cdc7775a6
Jay Bienvenu <bienvenunet@yahoo.com>
2016-12-16 13:43:21 -0600

    Implement SSN. Close #35.

--- chrome/options-complete.js
+++ chrome/options-complete.js
@@ -3029,9 +3029,22 @@ var UnitedStates = function(dd) {
     return zipCode()+"-"+digit()+digit()+digit()+digit();
   };
 
+  function ssn(options) {
+      var format = "###-##-####", result = "";
+      for (var i = 0; i < format.length; i++) {
+        if (format[i] == '#') {
+          result += Math.floor(Math.random()*10);
+        } else {
+          result += format[i];
+        }
+      }
+      return result;
+  }
+
   this.menuItems = [
     ["Zip Code", zipCode],
-    ["Zip+4 Code", zipPlus4]
+    ["Zip+4 Code", zipPlus4],
+    ["SSN",ssn]
   ];
 
 };
